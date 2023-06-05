bffcd9a63b7bd99cbbe953d7d92652f4c11c174f
Insoo Jeon <jis9107@naver.com>
2023-06-05 15:49:11 +0900

    첫번째 업로드
    포톤 네트워크 세팅 및 맵 세팅

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public InputField NickNameInput;
+    public GameObject DisconnectPanel;
+    public GameObject RespawnPanel;
+
+    private void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 2 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        DisconnectPanel.SetActive(false);
+        //Spawn();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected)
+            PhotonNetwork.Disconnect();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        DisconnectPanel.SetActive(true);
+        RespawnPanel.SetActive(false);
+
+    }
+    //public void Spawn()
+    //{
+    //    PhotonNetwork.Instantiate("RedKnight", Vector3.zero, Quaternion.identity);
+    //    RespawnPanel.SetActive(false);
+    //}
+}
+
