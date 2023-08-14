68824f7fcaef53c579e39b34dbe469df60a92682
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-08-14 23:14:45 +0900

    08/14

--- Assets/scripts/RedCamp.cs
+++ Assets/scripts/RedCamp.cs
@@ -7,16 +7,54 @@ using Photon.Realtime;
 
 public class RedCamp : MonoBehaviour
 {
+    public PhotonView pv;
+
     public Image healthImage;
+
+    public BoxCollider2D box;
+
+    bool isDamage;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "BlueMelee")
+        {
+            if (!isDamage)
+            {
+                Weapon weapon = other.GetComponent<Weapon>();
+                healthImage.fillAmount -= weapon.damage / 100f;
+                StartCoroutine("OnDamage");
+            }
+
+        }
+    }
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(healthImage.fillAmount);
+        }
+        else
+        {
+            healthImage.fillAmount = (float)stream.ReceiveNext();
+        }
+    }
+    IEnumerator OnDamage()
+    {
+        isDamage = true;
+        yield return new WaitForSeconds(1.3f);
+        isDamage = false;
     }
 }
