7b661f2de42bb40bd75d072317cee93552affc68
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-09-12 14:11:47 +0900

    09/12

--- Assets/scripts/sword.cs
+++ Assets/scripts/sword.cs
@@ -7,6 +7,7 @@ using Photon.Realtime;
 public class sword : MonoBehaviourPunCallbacks
 {
     public PhotonView pv;
+    public BoxCollider2D meleeArea;
 
     string _name;
 
@@ -18,26 +19,27 @@ public class sword : MonoBehaviourPunCallbacks
 
     void Update()
     {
-
+        
     }
 
 
     void OnTriggerEnter2D(Collider2D col) // col을 RPC의 매개변수로 넘겨줄 수 없다
     {
-        if (!pv.IsMine && col.tag == "Red") // 느린쪽에 맞춰서 Hit판정
+        if (!pv.IsMine && col.tag == "Red" && col.GetComponent<PhotonView>().IsMine) // 느린쪽에 맞춰서 Hit판정
         {
             _name = col.gameObject.name;
             Debug.Log(_name);
             switch(_name){
                 
-                case "RedKnight" :
+                case "RedKnight(Clone)" :
                     col.GetComponent<RedKnightScript>().Hit(10);
-                    this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                    meleeArea.enabled = false;
+
                     break;
 
-                case "RedPriest":
+                case "RedPriest(Clone)":
                     col.GetComponent<RedPriest>().Hit(10);
-                    this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                    meleeArea.enabled = false;
                     break;
 
                 default :
